a8267788297e8719739c58ef338004d8b22ad0d6
maddogdavis <mag@netstorm.net.au>
2013-05-15 18:14:27 +1000

    cokleisli compose complete

--- src/core/Hdd.hs
+++ src/core/Hdd.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Hole where
 
 class Functor f => SemiMonad f where
@@ -8,20 +6,11 @@ class Functor f => SemiMonad f where
 class Functor f => SemiComonad f where
   cobind :: (f a -> b) -> f a -> f b
 
-hole = undefined  -- "quiet" hole
-data Hole = Hole  -- "noisy" hole
-
---
-
 compose :: (b -> c) -> (a -> b) -> (a -> c)
 compose = (.)
 
---
-
 composeKleisli :: SemiMonad n => (b -> n c) -> (a -> n b) -> (a -> n c)
 composeKleisli f g x = bind f (g x)
-
--- step 3200 complete final tidy
 
 composeCokleisli :: SemiComonad n => (n b -> c) -> (n a -> b) -> (n a -> c)
 composeCokleisli f g x = f (cobind g x)
