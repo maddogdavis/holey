7a37c50ee84c9c5be24a6bd0f37e4ba728232831
maddogdavis <mag@netstorm.net.au>
2013-05-15 15:41:07 +1000

    2040

--- src/core/Hdd.hs
+++ src/core/Hdd.hs
@@ -13,11 +13,11 @@ data Hole = Hole  -- "noisy" hole
 compose :: forall a b c. (b -> c) -> (a -> b) -> (a -> c)
 compose = (.)
 
--- step 2030 introduce shadow bindings
+-- step 2040 seek result direction from "noisy" hole
   
 composeKleisli :: forall a b c n.
                   SemiMonad n => (b -> n c) -> (a -> n b) -> a -> n c
-composeKleisli f g x = hole
+composeKleisli f g x = Hole
   where
     _ = f :: b -> n c
     _ = g :: a -> n b
