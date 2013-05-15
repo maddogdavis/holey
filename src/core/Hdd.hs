eb7ec3b93faeab572dee40515e67c3b07032a43a
maddogdavis <mag@netstorm.net.au>
2013-05-15 15:42:51 +1000

    2050

--- src/core/Hdd.hs
+++ src/core/Hdd.hs
@@ -13,12 +13,14 @@ data Hole = Hole  -- "noisy" hole
 compose :: forall a b c. (b -> c) -> (a -> b) -> (a -> c)
 compose = (.)
 
--- step 2040 seek result direction from "noisy" hole
-  
+-- step 2050 introduce other goodies at our disposal
+
 composeKleisli :: forall a b c n.
-                  SemiMonad n => (b -> n c) -> (a -> n b) -> a -> n c
-composeKleisli f g x = Hole
+                  SemiMonad n => (b -> n c) -> (a -> n b) -> a -> n c 
+composeKleisli f g x = hole
   where
-    _ = f :: b -> n c
-    _ = g :: a -> n b
-    _ = x :: a
+    _ = bind :: (d -> n e) -> n d -> n e 
+    _ = fmap :: (d ->   e) -> n d -> n e 
+    _ = f    :: b -> n c 
+    _ = g    :: a -> n b 
+    _ = x    :: a
